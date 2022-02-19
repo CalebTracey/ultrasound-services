72abb0a060730e03f5224eb52c191d5e6ec4203d
Caleb Tracey <caleb.tracey@gmail.com>
2022-02-19 17:30:27 -0500

    added response structs for auth route

--- models/res/response.go
+++ models/res/response.go
@@ -1,5 +1,21 @@
 package res
 
 type UserAuthResponse struct {
-	Success bool
+	Success  bool
+	Username string
+	Message  Message
+}
+
+type Message struct {
+	Count     int
+	Status    string
+	TimeTaken string
+	ErrorLog  []ErrorLog
+	HostName  string
+}
+
+type ErrorLog struct {
+	RootCause  string
+	Trace      string
+	StatusCode string
 }
